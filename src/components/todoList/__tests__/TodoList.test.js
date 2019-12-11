3ff01e565f8d16240cdcc12a7ccdd5172edbd0ce
zhourusheng <nfuzrs@126.com>
2019-12-11 10:24:57 +0800

    生命周期 和 组件内部方法测试

--- src/components/todoList/__tests__/TodoList.test.js
+++ src/components/todoList/__tests__/TodoList.test.js
@@ -64,3 +64,41 @@ it('click item to be done', () => {
   wrapper.find('Button').at(0).simulate('click')
   expect(props.deleteTodo).toBeCalled()
 });
+
+
+// 测试组件生命周期
+
+// 使用spy替身时, 再测试用例结束后，要对spy进行 restore
+// 否则这个 spy 会一直存在，并且无法对相同的方法再次进行 spy
+it('calls componentDidMount', () => {
+  // 使用spyOn来mock 组件声明周期, 替身函数要在组件渲染之前，所有替身函数要定义在setup执行之前
+  const componentDidMountSpy = jest.spyOn(TodoList.prototype, 'componentDidMount')
+  const { wrapper } = setup()
+  expect(componentDidMountSpy).toHaveBeenCalled()
+  componentDidMountSpy.mockRestore()
+});
+
+
+// 测试组件内部函数
+
+// 使用 instance 函数来取得组件的实例，并用spyOn方法来 mock实例上的内部方法
+// 然后用这个实例去调用那个内部方法，就可以用替身来判断这个内部函数是否被调用
+
+it('calls component handleTest', () => {  // class 中使用箭头函数定义的方法
+  const { wrapper } = setup()
+  const spyFunction = jest.spyOn(wrapper.instance(), 'handleTest')
+  wrapper.instance().handleTest()
+  expect(spyFunction).toHaveBeenCalled()
+  spyFunction.mockRestore()
+});
+
+// 内部方法使用箭头函数定义时, 需要对实例 instance() 进行 mock
+// 如果内部方法通过正常方式 或者 bind() ,则需要使用 prototype 进行mock
+
+it('calls component handleTest2', () => {  // 在constructor使用bind 来定义的方法
+  const spyFunction = jest.spyOn(TodoList.prototype, 'handleTest2')
+  const { wrapper } = setup()
+  wrapper.instance().handleTest2()
+  expect(spyFunction).toHaveBeenCalled()
+  spyFunction.mockRestore()
+});
--- src/components/todoList/index.jsx
+++ src/components/todoList/index.jsx
@@ -2,6 +2,22 @@ import React from 'react'
 import { Button } from 'antd'
 
 class TodoList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleTest2 = this.handleTest2.bind(this)
+  }
+
+  componentDidMount() {
+    console.log('didMount')
+  }
+
+  handleTest() {
+    console.log('test')
+  }
+
+  handleTest2() {
+    console.log('test2')
+  }
 
   render() {
     const { list, deleteTodo } = this.props
